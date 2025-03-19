da1fe90c4345727eaa91890a472a3181ceb2e0ee
Shariq Choudhary <khanshariq92213@gmail.com>
2025-03-19 20:02:33 +0530

    New Route Added

--- controllers/hadith.js
+++ controllers/hadith.js
@@ -10,7 +10,8 @@ export const getHadithByNumber = async (req, res, next) => {
       return res.status(400).json({
         code: 400,
         status: "Error",
-        message: "Missing hadith parameter. Use '/editionLanguage-editionName' to get a hadith",
+        message:
+          "Missing hadith parameter. Use '/editionLanguage-editionName' to get a hadith",
       });
     }
 
@@ -19,13 +20,14 @@ export const getHadithByNumber = async (req, res, next) => {
       return res.status(400).json({
         code: 400,
         status: "Error",
-        message: "Invalid hadith format. Use 'editionLanguage-editionName' to get a hadith",
+        message:
+          "Invalid hadith format. Use 'editionLanguage-editionName' to get a hadith",
       });
     }
 
     const baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/${editionLanguage}-${editionName}/${number}.json`;
     const { data } = await axios.get(baseUrl);
-    
+
     res.json(data);
   } catch (error) {
     next(new ErrorHandler(500, error.message));
@@ -37,7 +39,7 @@ export const getAllEditions = async (req, res, next) => {
   try {
     const baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions.json`;
     const { data } = await axios.get(baseUrl);
-    
+
     res.json(data);
   } catch (error) {
     next(new ErrorHandler(500, error.message));
@@ -53,7 +55,8 @@ export const getSections = async (req, res, next) => {
       return res.status(400).json({
         code: 400,
         status: "Error",
-        message: "Missing hadith parameter. Use '/editionLanguage-editionName' to get a section",
+        message:
+          "Missing hadith parameter. Use '/editionLanguage-editionName' to get a section",
       });
     }
 
@@ -62,13 +65,14 @@ export const getSections = async (req, res, next) => {
       return res.status(400).json({
         code: 400,
         status: "Error",
-        message: "Invalid hadith format. Use 'editionLanguage-editionName' to get a section",
+        message:
+          "Invalid hadith format. Use 'editionLanguage-editionName' to get a section",
       });
     }
 
     const baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/${editionLanguage}-${editionName}/sections/${number}.json`;
     const { data } = await axios.get(baseUrl);
-    
+
     res.json(data);
   } catch (error) {
     next(new ErrorHandler(500, error.message));
@@ -80,7 +84,19 @@ export const getInfo = async (req, res, next) => {
   try {
     const baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/info.json`;
     const { data } = await axios.get(baseUrl);
-    
+
+    res.json(data);
+  } catch (error) {
+    next(new ErrorHandler(500, error.message));
+  }
+};
+
+export const getRandomHadith = async (req, res, next) => {
+  const { bookName } = req.query;
+  try {
+    const baseUrl = `https://random-hadith-generator.vercel.app/${bookName}`;
+    const { data } = await axios.get(baseUrl);
+
     res.json(data);
   } catch (error) {
     next(new ErrorHandler(500, error.message));
--- routes/hadith.js
+++ routes/hadith.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getHadithByNumber,getAllEditions, getSections,getInfo } from '../controllers/hadith.js';
+import { getHadithByNumber,getAllEditions, getSections,getInfo, getRandomHadith } from '../controllers/hadith.js';
 
 const router = express.Router();
 
 router.get('/editions', getAllEditions);
+router.get('/random', getRandomHadith)
 router.get('/info', getInfo)
 router.get('/section/:edition/:number', getSections);
 router.get('/:edition/:number', getHadithByNumber);
